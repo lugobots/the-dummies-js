aca0497db780b3b6dbfa2ac379f7d4c3fae22890
Rubens Silva <rubens21@gmail.com>
2024-03-16 09:42:58 -0600

    re arranging comments to guide devs

--- .dist/main.js
+++ .dist/main.js
@@ -1,19 +1,15 @@
 "use strict";
 exports.__esModule = true;
-var lugo4node_1 = require("@lugobots/lugo4node");
 var my_bot_1 = require("./my_bot");
+var lugo4node_1 = require("@lugobots/lugo4node");
 var starter = (0, lugo4node_1.NewDefaultStarter)();
-starter.run(new my_bot_1.MyBot(starter.getConfig().getBotTeamSide(), starter.getConfig().getBotNumber(), starter.getInitialPosition(), starter.getMapper()));
 // ADVANCED
 // the map will help us to see the field in quadrants (called regions) instead of working with coordinates
+//
 // const config = new EnvVarLoader()
 // the map will help us to see the field in quadrants (called regions) instead of working with coordinates
 // const map = new Mapper(10, 6, config.getBotTeamSide())
 // our bot strategy defines our bot initial position based on its number
-// const initialRegion = map.getRegion(DEFAULT_PLAYER_POSITIONS[config.getBotNumber()].Col, DEFAULT_PLAYER_POSITIONS[config.getBotNumber()].Row)
-// const lugoClient = NewClientFromConfig(config, initialRegion.getCenter())
-// lugoClient.playAsBot(bot).then(() => {
-//   console.log(`all done`)
-// }).catch(e => {
-//   console.error(e)
-// })
+// import {PLAYER_INITIAL_POSITIONS} from "./settings";
+// const initialRegion = map.getRegion( PLAYER_INITIAL_POSITIONS[config.getBotNumber()].Col, PLAYER_INITIAL_POSITIONS[config.getBotNumber()].Row)
+starter.run(new my_bot_1.MyBot(starter.getConfig().getBotTeamSide(), starter.getConfig().getBotNumber(), starter.getInitialPosition(), starter.getMapper()));
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
-import { NewDefaultStarter } from '@lugobots/lugo4node'
 import { MyBot } from './my_bot'
+import {EnvVarLoader, Mapper, NewDefaultStarter} from '@lugobots/lugo4node'
+
 
 const starter = NewDefaultStarter()
+
+// ADVANCED
+// the map will help us to see the field in quadrants (called regions) instead of working with coordinates
+//
+// const config = new EnvVarLoader()
+
+// the map will help us to see the field in quadrants (called regions) instead of working with coordinates
+// const map = new Mapper(10, 6, config.getBotTeamSide())
+
+// our bot strategy defines our bot initial position based on its number
+// import {PLAYER_INITIAL_POSITIONS} from "./settings";
+// const initialRegion = map.getRegion( PLAYER_INITIAL_POSITIONS[config.getBotNumber()].Col, PLAYER_INITIAL_POSITIONS[config.getBotNumber()].Row)
 
 starter.run(new MyBot(
   starter.getConfig().getBotTeamSide(),
@@ -10,20 +23,3 @@ starter.run(new MyBot(
   starter.getMapper()
 ));
 
-// ADVANCED
-// the map will help us to see the field in quadrants (called regions) instead of working with coordinates
-// const config = new EnvVarLoader()
-
-// the map will help us to see the field in quadrants (called regions) instead of working with coordinates
-// const map = new Mapper(10, 6, config.getBotTeamSide())
-
-// our bot strategy defines our bot initial position based on its number
-// const initialRegion = map.getRegion(DEFAULT_PLAYER_POSITIONS[config.getBotNumber()].Col, DEFAULT_PLAYER_POSITIONS[config.getBotNumber()].Row)
-
-// const lugoClient = NewClientFromConfig(config, initialRegion.getCenter())
-
-// lugoClient.playAsBot(bot).then(() => {
-//   console.log(`all done`)
-// }).catch(e => {
-//   console.error(e)
-// })
